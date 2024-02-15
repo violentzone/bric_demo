5a7299d34bbbe360eec31aa606426cd871638eb5
Yin <yinchen@mitac.com.tw>
2024-02-15 09:34:45 +0800

    Commit and switch to another branch

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, JWTManager, jwt_required
+
+from utils import db_util
 
 
 app = Flask(__name__)
+jwt = JWTManager()
 CORS(app)
+app.config['JWT_SECRET_KEY'] = 'temp_secrete_key'
+jwt.init_app(app)
 
 @app.route('/login', methods=['POST'])
 def login():
-    print(request.json)
-    return 'OK'
+    operator = db_util.DbOperator()
+    username = request.json['username']
+    password = request.json['password']
+    if operator.login_check(username, password):
+        access_token = create_access_token(identity=username)
+        return jsonify(msg="Login passed", access_token=access_token)
+    else:
+        return jsonify(msg="Invalid password"), 401
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ utils/db_util.py
@@ -0,0 +1,74 @@
+""" Utility of login, containing create, verify passwords etc"""
+import pymysql
+from json import loads
+from passlib.hash import pbkdf2_sha256
+from datetime import datetime
+
+class DbOperator:
+    def __init__(self):
+
+        """
+        Init, check if able to login and create database 'leavesystem' if not exists
+        """
+        # Get json config
+        with open('infos/db.json') as f:
+            db_config = loads(f.read())
+        self.connection = pymysql.connect(host=db_config['url'], user=db_config['user'], password=db_config['password'])
+        if_database ="""
+        CREATE DATABASE IF NOT EXISTS leavesystem;"""
+        if_table="""
+        CREATE TABLE IF NOT EXISTS leavesystem.users (
+        ID BIGINT NOT NULL AUTO_INCREMENT,
+        userName text,
+        hashpassword varchar(255),
+        createdate datetime,
+        PRIMARY KEY (ID) 
+        );
+        """
+        with self.connection.cursor() as cursor:
+            cursor.execute(if_database)
+            cursor.execute(if_table)
+            self.connection.commit()
+
+
+    def login_check(self, user_name: str, password: str) -> bool:
+        user_info_sql = """
+        SELECT userName, hashpassword FROM leavesystem.users where userName = %s
+        """
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(user_info_sql, user_name)
+                user_info = cursor.fetchone()
+        except:
+            ValueError('Cannot query user information during login check')
+        # Check if get anything
+        if user_info is None:
+            return False
+
+        # Check if password can verify
+        if pbkdf2_sha256.verify(password, user_info[1]):
+            return True
+        else:
+            return False
+
+    def create_user(self, user_name: str, password: str) -> bool:
+        # Check if any identical userName in db
+        check_exists_sql = """
+        SELECT EXISTS (SELECT userName FROM leavesystem.users where userName = %s)
+        """
+        with self.connection.cursor() as cursor:
+            cursor.execute(check_exists_sql, user_name)
+            check_exists = cursor.fetchone()
+
+        if check_exists:
+            return False
+        else:
+            password_hash = pbkdf2_sha256.hash(password, salt=b'the_salt')
+            create_time = datetime.now()
+            insert_user_sql = """
+            INSERT INTO leavesystem.users (userName, hashpassword, createdate) VALUES(%s, %s, %s)"""
+
+            with self.connection.cursor() as cursor:
+                cursor.execute(insert_user_sql, (user_name, password_hash, create_time))
+                self.connection.commit()
+            return True
