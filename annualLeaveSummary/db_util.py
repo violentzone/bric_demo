c99e5c0ae9abca331c009f4c3b8d4018b37cc666
violentzone <farewell.torture@gmail.com>
2024-06-11 01:27:09 +0800

    Init of summary, contains add ID

--- annualLeaveSummary/db_util.py
+++ annualLeaveSummary/db_util.py
@@ -1,14 +1,16 @@
-import pymysql
+from datetime import datetime
 from global_util.connection_pool import POOL
 
 
 class DbOperator:
     def __init__(self):
         connection = POOL.connection()
-
+        this_year = datetime.now()
         self.connection = connection
 
     def get_init_expire(self):
         init_expire_sql = """
-            SELECT 
+            SELECT given, expire from leavesystem.leaveleft
+            WHERE leave_type = 1
+            AND expire 
             """
