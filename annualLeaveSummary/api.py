a47e7c89a6670fdd046c33f2c0e2182f78c9d210
Yin <yinchen@mitac.com.tw>
2024-06-13 17:07:46 +0800

    Get data page of summary is finished

--- annualLeaveSummary/api.py
+++ annualLeaveSummary/api.py
@@ -1,4 +1,4 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, jsonify
 from annualLeaveSummary.db_util import DbOperator
 
 annual_leave_summaryApp = Blueprint('annualLeaveBp', __name__)
@@ -9,5 +9,6 @@ operator = DbOperator()
 def get_full_data():
     user_id = request.json['user_id']
     given, expire = operator.get_init_expire(user_id)
-    print('api', given, expire)
-    return {}
+    data = operator.get_frame_data(user_id)
+    full_data = {'given': given, 'expire': expire, 'data': data}
+    return jsonify(full_data)
--- annualLeaveSummary/db_util.py
+++ annualLeaveSummary/db_util.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from global_util.connection_pool import POOL
-
+from global_util.global_util import get_leavetype_collate
 
 
 class DbOperator:
@@ -11,6 +11,16 @@ class DbOperator:
         self.current_year = datetime.today().year
 
     def get_init_expire(self, user_id) -> tuple:
+        """
+        Function to get the last exprie date fo current year's annual leave
+        Parameters
+        ----------
+        user_id: [int] The user to query from
+
+        Returns
+        -------
+        Annual leave given date, Annual leave expire date
+        """
         init_expire_sql = """
             SELECT given, expire from leavesystem.leaveleft
             WHERE leave_type = 1
@@ -31,6 +41,30 @@ class DbOperator:
             return None, None
 
     def get_frame_data(self, user_id: int):
+        """
+        The data of all used leaves
+        Parameters
+        ----------
+        user_id: [int] The user id to query from
+
+        Returns
+        -------
+        Json of the data
+        """
         annual_leave_summary = """
-            SELECT start_time, end_time, duration, 
+            SELECT start_time, end_time, hours, leave_type, reason
+            FROM leavesystem.leaveused
+            WHERE userID = %s
             """
+        with self.connection.cursor() as cursor:
+            cursor.execute(annual_leave_summary, user_id)
+            data = cursor.fetchall()
+
+        with self.connection.cursor() as cursor:
+            leavetype_dict = get_leavetype_collate(cursor, 'chinese')
+        # Convert data to desired type
+        formatted_data = [
+            (leave[0].strftime('%Y-%m-%d'), leave[1].strftime('%Y-%m-%d'), leave[2], leavetype_dict[leave[3]], leave[4]) for leave
+            in data]
+
+        return formatted_data
