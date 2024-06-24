f0b4f2b571f1d46ff949f15567de8c89f3a42ccf
violentzone <farewell.torture@gmail.com>
2024-06-25 00:49:10 +0800

    Just tested and make sure that works

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from datetime import timedelta
 from login import *
 from home import *
 from create import *
@@ -11,6 +12,7 @@ app = Flask(__name__)
 jwt = JWTManager()
 CORS(app)
 app.config['JWT_SECRET_KEY'] = 'temp_secrete_key'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=30)  # Token expires in 30 minutes
 jwt.init_app(app)
 
 app.register_blueprint(loginApp)
--- home/api.py
+++ home/api.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request, jsonify
 from home import db_util
+from flask_jwt_extended import jwt_required
 
 homeApp = Blueprint('homeBp', __name__)
 operator = db_util.DbOperator()
 
+
 @homeApp.route('/formscount', methods=['POST'])
+@jwt_required()
 def formscount():
     userid = request.json['user_id']
     form_count = operator.unsign_num(userid)
